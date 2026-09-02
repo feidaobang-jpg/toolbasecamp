462fa0b2d0fd680ccd50dc25501578065c41c45a
zhengxiaohui <378180078@qq.com>
2026-09-02 10:13:34 +0800

    删除自启动

--- comfyui-api-server/uninstall-comfyui-autostart.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-chcp 65001 >nul
-set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\ComfyUI-Main.vbs"
-if exist "%DEST%" del /f "%DEST%"
-echo Removed ComfyUI autostart (if it existed).
-pause
-endlocal
